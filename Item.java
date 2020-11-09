657f094e5c869518f80de6ab69587af201663bc0
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2020-11-08 19:45:44 -0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,5 +1,10 @@
 package sample;
+/********************************************************************
+By Jacob V
 
+This is the item class. At the moment, the only member variables is a description
+of the item. More member variables can be added later.
+********************************************************************/
 public class Item {
 
     private String description;
@@ -14,6 +19,6 @@ public class Item {
 
         description = new_description;
     }
-
+    //Returns the description data memeber
     public String getDescription(){ return description; }
 }
