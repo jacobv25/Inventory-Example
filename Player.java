d136670a54d36b67022241851a1c914187fb27e3
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2020-11-08 19:47:58 -0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,8 +2,12 @@ package sample;
 
 import javafx.scene.paint.Color;
 import sample.Inventory;
-
 import sample.Inventory;
+/******************************************************************************
+By Jacobo V
+
+The Player class
+******************************************************************************/
 
 public class Player {
 
@@ -17,12 +21,17 @@ public class Player {
         level = 1;
         inv = new Inventory();
     }
-
+    /**************************************
+    Displays the player's inventory
+    *************************************/
     public void showInventory(){
 
         inv.showInventory();
     }
-
+    /******************************************
+    Adds an item to the players inventory
+    Param: newItem - The Item object to be added.
+    *****************************************/
     public void addItemToInventory(Item newItem) {
 
         inv.addItem(newItem);
